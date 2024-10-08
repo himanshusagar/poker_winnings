fb7bfd5788ed71805765e69f5a759026134cdeb4
Himanshu Sagar <himanshusagar@protonmail.com>
2024-10-08 12:07:15 -0700

    Initial Commit

---
+++ PokerWinnings/PokerWinningsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokerWinningsApp.swift
+//  PokerWinnings
+//
+//  Created by Himanshu Sagar on 10/8/24.
+//
+
+import SwiftUI
+
+@main
+struct PokerWinningsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PokerWinningsTests/PokerWinningsTests.swift
@@ -0,0 +1,17 @@
+//
+//  PokerWinningsTests.swift
+//  PokerWinningsTests
+//
+//  Created by Himanshu Sagar on 10/8/24.
+//
+
+import Testing
+@testable import PokerWinnings
+
+struct PokerWinningsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
